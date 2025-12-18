6e3fab4e33695b90c7e758b531bfc4236f7814f0
Manish Surabhi <manishsurabhi23@gmail.com>
2025-12-17 20:26:15 -0500

    staged changes

---
+++ data_ingestion/data_transform.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import langchain_core.documents import Document
+
+class data_converter:
+    def __init__(self):
+        pass
+
+    def data_transformation(self):
+        pass
